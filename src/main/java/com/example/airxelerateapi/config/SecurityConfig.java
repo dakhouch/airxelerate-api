9c0974e7bacc2f4cbceae42cd10fafcff6ee38fb
ADA <anass.dakhouch@outlook.com>
2025-02-24 07:48:04 +0000

    add security config

--- src/main/java/com/example/airxelerateapi/config/SecurityConfig.java
+++ src/main/java/com/example/airxelerateapi/config/SecurityConfig.java
@@ -1,7 +1,47 @@
 package com.example.airxelerateapi.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${allowed.origin:http://localhost:3000}")
+    private String allowedOrigin;
+    // form login deprecated
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated());
+//                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+       http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Access-Control-Allow-Origin", "Content-Disposition");
+
+            }
+        };
+    }
 
 }
