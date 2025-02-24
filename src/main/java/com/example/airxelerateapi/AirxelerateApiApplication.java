ca3f871d93ad424ee7bef938a6957931e162c1df
ADA <anass.dakhouch@outlook.com>
2025-02-24 07:04:37 +0000

    boostraping project

---
+++ src/main/java/com/example/airxelerateapi/AirxelerateApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.airxelerateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirxelerateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirxelerateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airxelerateapi/AirxelerateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airxelerateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirxelerateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
